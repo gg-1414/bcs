09606e239747867c781df28e161103b257210b47
brittjohn87 <brittanytjohnson@icloud.com>
2020-11-03 18:00:11 -0500

    changed input text color

--- src/components/input.js
+++ src/components/input.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Input = ({ type, name, value, onChange, placeholder }) => (
     <>
         <input
-            style={{ 'background': 'black', 'border': '1px solid white' }}
+            style={{ 'background': 'black', 'border': '1px solid white', 'color': 'white' }}
             className="form-control"
             name={name}
             type={type}
