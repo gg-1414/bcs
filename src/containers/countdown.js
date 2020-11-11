427256ea7aec50b2875044ffc8ce70a0e9dc355b
yourmobilegeek <hello@yourmobilegeek.tech>
2020-11-11 06:36:57 -0800

    cleaned up countdown styling

--- src/containers/countdown.js
+++ src/containers/countdown.js
@@ -1,9 +1,10 @@
 import React from "react";
 import CountdownTimer from "react-component-countdown-timer";
 
+
 export const Countdown = () => (
   <div className="d-flex justify-content-center">
-    <CountdownTimer count={5432} showTitle size={40} labelSize={20} border />
+    <CountdownTimer count={1400000} showTitle dayTitle={"Days"} responsive size={40} labelSize={14} color="#fff" backgroundColor="#000" />
   </div>
 );
 
