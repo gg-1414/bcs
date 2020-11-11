0da50b6c628fc2ced5c162a650a53158473a1040
yourmobilegeek <hello@yourmobilegeek.tech>
2020-11-11 06:16:00 -0800

    countdown timer & favicon updates

---
+++ src/containers/countdown.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CountdownTimer from "react-component-countdown-timer";
+
+export const Countdown = () => (
+
+class SimpleCountdownTimer extends React.Component {
+  render() {
+    return (
+        <CountdownTimer count={5432} showTitle size={40} labelSize={20} border />
+    );
+  }
+}
+
+);
+
