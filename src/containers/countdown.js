3f720081bccfc050a04e6ed2f0a54bc63582e8b4
brittjohn87 <brittanytjohnson@icloud.com>
2020-11-11 10:28:03 -0500

    adjusted countdown clock

--- src/containers/countdown.js
+++ src/containers/countdown.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import CountdownTimer from "react-component-countdown-timer";
 
 export const Countdown = () => (
   <div className="d-flex justify-content-center">
-  <Countdown date={("2020-11-27")} />
+    <CountdownTimer count={1400000} showTitle dayTitle={"Days"} responsive size={40} labelSize={14} color="#fff" backgroundColor="#000" />
   </div>
 );
 
