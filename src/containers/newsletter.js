08aa18ac4930a692a228b137095d00c06a10c649
brittjohn87 <brittanytjohnson@icloud.com>
2020-11-11 22:39:33 -0500

    fixed mobile form

--- src/containers/newsletter.js
+++ src/containers/newsletter.js
@@ -4,8 +4,8 @@ import { Signup } from '../components/signup';
 export const Newsletter = () => (
     <>
         <div className="d-block d-sm-none">
-            <div className="d-flex flex-column justify-content-center align-items-center p-4">
-                <h4>Wanna be on the list?</h4>
+            <div className="d-flex flex-column justify-content-center align-items-center mobile-form pt-3 pb-3">
+                <p>Wanna be on the list?</p>
                 <Signup />
             </div>
         </div>
