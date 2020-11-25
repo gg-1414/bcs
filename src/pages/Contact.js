6a1c9aa84f3ac0788985536b967937d35e8556b2
Gina Lee <gina@thesistesting.com>
2020-11-24 23:23:25 -0500

    contact page update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,43 +1,15 @@
 import React from 'react';
-import ContactImg from '../assets/images/about.jpg';
+import IGIcon from '../assets/images/ig.svg';
 import '../styles/pages/Contact.scss';
-
-function onSubmit(event) {
-  event.preventDefault()
-  console.log('onsubmit')
-}
 
 const Contact = () => {
   return (
     <section className="Contact">
       <div className="container">
-        <div className="image-block">
-          <img src={ContactImg} alt="Contact page" />
-        </div>
+        <a href="https://www.instagram.com/bestcasescenarionyc">
+          <img src={IGIcon} alt="Instagram icon" />
+        </a>
         <p className="email">info@bestcasescenario.nyc</p>
-        <form onSubmit={onSubmit}>
-          <input 
-            type="text"
-            name="name"
-            placeholder="name"
-          />
-          <input 
-            type="text"
-            name="email"
-            placeholder="email"
-          />
-          <input 
-            type="text"
-            name="subject"
-            placeholder="subject"
-          />
-          <textarea 
-            type="text"
-            name="message"
-            placeholder="message"
-          />
-          <button>Submit</button>
-        </form>
       </div>
     </section>
   )
