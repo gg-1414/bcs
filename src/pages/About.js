2d994388e7d041db8a4ccef0d44251af11fd6d65
Gina Lee <gina@thesistesting.com>
2020-11-27 02:53:10 -0500

    replace abouto img

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import aboutImg from '../assets/images/about.jpg';
+import aboutImg from '../assets/images/about.png';
 import '../styles/pages/About.scss';
 
 const About = () => {
