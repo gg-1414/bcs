ecb9ec477899af8447b4a68f0e365bfe6830ecd4
Gina Lee <gina@thesistesting.com>
2020-11-27 19:22:57 -0500

    add thank you page[

---
+++ src/pages/ThankYou.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/pages/ThankYou.scss';
+
+const ThankYou = () => {
+  return (
+    <section className="ThankYou">
+      <p>BET ;)</p>
+    </section>
+  )
+}
+
+export default ThankYou;
